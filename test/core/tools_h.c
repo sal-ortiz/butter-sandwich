50708d6c7b9a2a7e7587027272a27adef9eea33c
sal ortiz <this.is.salvador.ortiz@gmail.com>
2021-11-09 22:41:35 -0600

    cleaned up tools_h test

--- test/core/tools_h.c
+++ test/core/tools_h.c
@@ -6,48 +6,58 @@
 #define _TEST_HASHCODE_NUMBER_OF_PASSES   0xf
 
 
-
-
-
 bool test_hashCode(){
+  unsigned long int* hashCodeList;
+  unsigned char numPasses = _TEST_HASHCODE_NUMBER_OF_PASSES;
 
-  // populate our list, verifying entries per pass.
-  unsigned long int* hashCodeList = new unsigned long int[_TEST_HASHCODE_NUMBER_OF_PASSES];
-  for( unsigned long int generatorIndex = 1; generatorIndex <= _TEST_HASHCODE_NUMBER_OF_PASSES; generatorIndex++ ){
-    hashCodeList[ generatorIndex ] = hashCode( int_to_str(generatorIndex) );
-    assert( hashCodeList[ generatorIndex ] == hashCode( int_to_str(generatorIndex) ) );
+  hashCodeList = new unsigned long int[_TEST_HASHCODE_NUMBER_OF_PASSES];
+
+  for (unsigned long int genIdx = 1; genIdx <= numPasses; genIdx++) {
+    const char* genIdxStr = int_to_str(genIdx);
+
+    hashCodeList[genIdx] = hashCode(genIdxStr);
+    assert( hashCodeList[genIdx] == hashCode(genIdxStr) );
   }
 
-  // verify contents, validating for repetitions.
-  for( unsigned long int baseIndex = 1; baseIndex <= _TEST_HASHCODE_NUMBER_OF_PASSES; baseIndex++ ){
+
+  for (unsigned long int baseIdx = 1; baseIdx <= numPasses; baseIdx++) {
     unsigned long int matchCount = 0;
-    for( unsigned long int compareIndex = 1; compareIndex <= _TEST_HASHCODE_NUMBER_OF_PASSES; compareIndex++ )
-      if( hashCode( int_to_str(baseIndex) ) == hashCodeList[compareIndex] )   matchCount++;
+
+    for (unsigned long int cmpIdx = 1; cmpIdx <= numPasses; cmpIdx++) {
+      const char* baseIdxStr = int_to_str(baseIdx);
+
+      if (hashCode(baseIdxStr) == hashCodeList[cmpIdx]) {
+        matchCount++;
+      }
+
+    }
 
     assert( matchCount <= 1 );
   }
 
   return true;
-} // ---------------------------------------------------------------------------
-
+}
 
 bool test_array(){
-
   int* ary;
-  ary = (int*)array( 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 );
-  for( unsigned long int index = 0; index < 10; assert( ary[index] == index++ ) );
 
-  ary = (int*)array( 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 );  // fill the array.
-  for( unsigned long int index = 0; index < 10; assert( ary[index] == 9-index++ ) );
+  ary = (int*)array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+  for (unsigned long int idx = 0; idx < 10; idx++) {
+    assert( ary[idx] == idx );
+  }
+
+  ary = (int*)array(9, 8, 7, 6, 5, 4, 3, 2, 1, 0);
+  for (unsigned long int idx = 0; idx < 10; idx++) {
+    assert( ary[idx] == 9 - idx );
+  }
 
   return true;
-} // ---------------------------------------------------------------------------
-
+}
 
 
 int main( int argc, char *argv[] ){
 
-  //assert( test_hashCode() == true );
+  assert( test_hashCode() == true );
   assert( test_array() == true );
 
   return 0x0000;  // EXIT_SUCCESS;
