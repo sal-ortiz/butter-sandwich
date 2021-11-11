a5a3f800339784ce2ae2837a64a6092c0fc5c36c
sal ortiz <this.is.salvador.ortiz@gmail.com>
2021-11-11 15:50:35 -0600

    added shared container base

---
+++ src/core/container_base.hpp
@@ -0,0 +1,15 @@
+
+#ifndef _CONTAINERBASE_HPP_
+  #define _CONTAINERBASE_HPP_
+
+  #include <math.h>
+
+  #include "./tools.h"
+  #include "./base.hpp"
+
+
+  class ContainerBase: public Base {
+    // blank!
+  };
+
+#endif  // #ifndef _CONTAINERBASE_hpp_
---
+++ test/core/container_base_hpp.cpp
@@ -0,0 +1,9 @@
+
+#include <assert.h>
+#include "../../src/core/container_base.hpp"
+
+
+int main( int argc, char *argv[] ){
+
+  return 0x0000;  // EXIT_SUCCESS;
+}
