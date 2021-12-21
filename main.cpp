180d52503907eafa5e4410bc374bde55ee3939e2
sal ortiz <this.is.salvador.ortiz@gmail.com>
2021-12-20 21:12:30 -0600

    updated main for testing

--- main.cpp
+++ main.cpp
@@ -1,16 +1,26 @@
 
+
+
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
 #include "./src/runtime/window.hpp"
 #include "./src/runtime/image.hpp"
+#include "./src/runtime/event.hpp"
+#include "./src/application.hpp"
 
 #include "./src/sprite.hpp"
 
+#include "./src/core/dict.hpp"
+
+
+Window* win = new Window();
+Application* app = new Application();
+
 
 int main(int argc, char *argv[]) {
+  bool isRunning = true;
 
-  Window* win = new Window();
   Sprite* sprite = new Sprite();
 
   win->open("The window!", 50, 50, 480, 360);
@@ -20,13 +30,16 @@ int main(int argc, char *argv[]) {
   img->load("./img.bmp", 0, 0);
   sprite->addFrame(img);
 
+  while (app->evaluate()) {
 
-  sprite->render(win->getRenderer(), 0, 0);
+    sprite->render(win->getRenderer(), 0, 0);
 
-  win->render();
+    win->render();
+  }
 
-  SDL_Delay(5000);
+  //SDL_Delay(5000);
 
-  win->close();
+  delete win;
 
+  SDL_Quit();
 }
