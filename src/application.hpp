b88cd88cd164c29cac0daefcb7ed614057cb74af
sal ortiz <this.is.salvador.ortiz@gmail.com>
2021-12-20 20:21:34 -0600

    added base app lib

---
+++ src/application.hpp
@@ -0,0 +1,39 @@
+
+#include "./runtime/event.hpp"
+
+
+
+class Application {
+
+  private:
+
+    Event* events;
+
+
+  public:
+
+    bool isRunning;
+
+    Application() {
+      this->isRunning = true;
+      this->events = new Event();
+
+      this->events->on("CLOSE", quitCallback);
+      this->events->on("QUIT", quitCallback);
+    }
+
+    bool evaluate() {
+      return Event::poll();
+    }
+
+    static void on(const char* id, bool (*callback)()) {
+      Event::on(id, callback);
+    }
+
+    static bool quitCallback() {
+      printf("QUITTING!!!!\n");
+
+      return false;
+    }
+
+};
