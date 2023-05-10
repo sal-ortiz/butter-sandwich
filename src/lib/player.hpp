5294a31ff3653a94555c803d7efc728816ef5b93
Sal Ortiz <this.is.salvador.ortiz@gmail.com>
2023-05-10 13:08:40 -0500

    tweaked trajectory settings for our player

--- src/lib/player.hpp
+++ src/lib/player.hpp
@@ -13,13 +13,13 @@ class Player: public SceneCharacter {
     Player() {
       Trajectory* traj = (Trajectory*)this->state->get("trajectory");
 
-      traj->positionRate.horz = 0.90;
-      traj->positionRate.vert = 0.90;
-      traj->positionRate.depth = 0.90;
+      traj->positionRate.horz = 0.925;
+      traj->positionRate.vert = 0.925;
+      traj->positionRate.depth = 0.925;
 
-      traj->angleRate.pitch = 0.90;
-      traj->angleRate.roll = 0.90;
-      traj->angleRate.yaw = 0.90;
+      traj->angleRate.pitch = 0.85;
+      traj->angleRate.roll = 0.85;
+      traj->angleRate.yaw = 0.85;
 
       traj->scaleRate.horz = 0.90;
       traj->scaleRate.vert = 0.90;
