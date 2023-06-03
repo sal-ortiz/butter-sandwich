b6d6e65c72f2bd78f926e873199e4cb51186645e
Sal Ortiz <this.is.salvador.ortiz@gmail.com>
2023-06-03 12:21:30 -0500

    tweaked player's starting point

--- main.cpp
+++ main.cpp
@@ -171,8 +171,8 @@ int main(int argc, char *argv[]) {
   scene->view->position.vert = round((scene->size->vert / 2) - (scene->view->size.vert / 2));
 
   player->state->set("absolute_position", new Position(
-    (scene->size->horz / 2) - (scene->view->size.horz / 2) - (player->width / 2),
-    (scene->size->vert / 2) - (scene->view->size.vert / 2) - (player->width / 2)
+    (scene->size->horz / 2) - (player->width / 2),
+    (scene->size->vert / 2) - (player->width / 2)
   ));
 
   Position* playerAbsolutePos = (Position*)player->state->get("absolute_position");
