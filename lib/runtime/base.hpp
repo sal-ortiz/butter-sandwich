b3aea0e1446f86f0018f9170a346b3910553cc7c
Sal Ortiz <this.is.salvador.ortiz@gmail.com>
2024-01-02 18:23:50 -0600

    added a string id to runtime elements

--- lib/runtime/base.hpp
+++ lib/runtime/base.hpp
@@ -7,6 +7,7 @@
   #include <stdint.h>
   #include <time.h>
   #include <stdlib.h>
+  #include <string.h>
 
   #include "./hook.hpp"
   #include "../tools/identifier.hpp"
@@ -16,17 +17,24 @@
 
     private:
 
-      uint32_t identifier;
+      uint32_t intIdentifier;
+      char strIdentifier[10];
 
 
     public:
 
       RuntimeBase() {
-        this->identifier = IdentifierTools::generate();
+        this->intIdentifier = IdentifierTools::generate();
+
+        sprintf(this->strIdentifier, "%u", this->intIdentifier);
       }
 
       uint32_t getIdentifier() {
-        return this->identifier;
+        return this->intIdentifier;
+      }
+
+      char* getIdentifierString() {
+        return this->strIdentifier;
       }
 
       void on(const char* id, void*(*callback)(void*, void*, void*), void* dataOne=NULL, void* dataTwo=NULL) {
