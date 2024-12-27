da19473da7f89bcd42b63ec992d365ae776b36aa
= <this.is.salvador.ortiz@gmail.com>
2024-12-27 00:39:31 -0600

    added a work-in-progress binary tree list

---
+++ lib/core/list/binary_tree_list.hpp
@@ -0,0 +1,191 @@
+
+
+#ifndef _BINARY_TREE_LIST
+
+  #define _BINARY_TREE_LIST
+
+  #include <stddef.h>
+  #include "binary_tree_list/node.hpp"
+
+  #include <stdio.h>
+
+
+
+  template <class class_type>
+  class BinaryTreeList {
+
+    private:
+
+      BinaryTreeListNode<class_type>* root;
+
+      uint32_t length;
+
+
+      BinaryTreeListNode<class_type>* getEntry(uint32_t targIndex) {
+
+        if (this->root == NULL) {
+          return NULL;
+        }
+
+        BinaryTreeListNode<class_type>* curNode = this->root;
+        uint32_t curIndex = curNode->getIndex();
+
+        while (curNode != NULL && curIndex != targIndex) {
+          curIndex = curNode->getIndex();
+
+          if (targIndex < curIndex) {
+            curNode = curNode->getLeft();
+          } else if (targIndex > curIndex) {
+            curNode = curNode->getRight();
+          }
+
+          if (curNode == NULL) {
+            return NULL;
+          }
+
+          curIndex = curNode->getIndex();
+        }
+
+        return curNode;
+      }
+
+      void setEntry(uint32_t targIndex, class_type value) {
+
+        if (targIndex >= this->length) {
+          this->length = targIndex + 1;
+        }
+
+        if (this->root == NULL) {
+          this->root = new BinaryTreeListNode<class_type>();
+
+          this->root->setIndex(targIndex);
+          this->root->setValue(value);
+
+          return;
+        }
+
+        BinaryTreeListNode<class_type>* prevNode = NULL;
+        BinaryTreeListNode<class_type>* curNode = this->root;
+
+        while (curNode != NULL) {
+
+          if (targIndex == curNode->getIndex()) {
+            curNode->setValue(value);
+
+            return;
+          }
+
+          if (targIndex < curNode->getIndex()) {
+            prevNode = curNode;
+            curNode = curNode->getLeft();
+
+          } else if (targIndex > curNode->getIndex()) {
+            prevNode = curNode;
+            curNode = curNode->getRight();
+
+          }
+
+        }
+
+        if (targIndex < prevNode->getIndex()) {
+          BinaryTreeListNode<class_type>* newNode = new BinaryTreeListNode<class_type>();
+
+          newNode->setIndex(targIndex);
+          newNode->setValue(value);
+
+          prevNode->setLeft(newNode);
+
+        } else if (targIndex > prevNode->getIndex()) {
+          BinaryTreeListNode<class_type>* newNode = new BinaryTreeListNode<class_type>();
+
+          newNode->setIndex(targIndex);
+          newNode->setValue(value);
+
+          prevNode->setRight(newNode);
+        }
+
+      }
+
+      void insertEntry(uint32_t targIndex, class_type value) {
+        BinaryTreeListNode<class_type>* node = this->root;
+
+        this->insertEntry(node, targIndex, value);
+        this->setEntry(targIndex, value);
+
+        this->length++;
+      }
+
+      void insertEntry(BinaryTreeListNode<class_type>* node, uint32_t targIndex, class_type value) {
+
+        if (node == NULL) {
+          return;
+        }
+
+
+        uint32_t nodeIdx = node->getIndex();
+
+        if (nodeIdx >= targIndex) {
+          node->setIndex(nodeIdx + 1);
+        }
+
+        BinaryTreeListNode<class_type>* leftChild = node->getLeft();
+        BinaryTreeListNode<class_type>* rightChild = node->getRight();
+
+        this->insertEntry(leftChild, targIndex, value);
+        this->insertEntry(rightChild, targIndex, value);
+      }
+
+      void deleteEntry(uint32_t targIndex) {
+        // TODO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!111
+      }
+
+
+    public:
+
+
+      BinaryTreeList() {
+        this->root = NULL;
+        this->length = 0;
+      }
+
+      //~StaticTreeList() {
+      //
+      //}
+
+      uint32_t getLength() {
+        return this->length;
+      }
+
+      class_type get(uint32_t targIndex) {
+        BinaryTreeListNode<class_type>* node = this->getEntry(targIndex);
+
+        if (node != NULL) {
+          return node->getValue();
+        }
+
+        return NULL;
+      }
+
+      void set(uint32_t targIndex, class_type value) {
+        this->setEntry(targIndex, value);
+      }
+
+      void remove(uint32_t targIndex) {
+        this->deleteEntry(targIndex);
+      }
+
+      void push(class_type value) {
+        uint32_t index = this->length;
+
+        this->setEntry(index, value);
+      }
+
+      void unshift(class_type value) {
+        uint32_t index = 0;
+
+        this->insertEntry(index, value);
+      }
+
+  };
+
+#endif
---
+++ lib/core/list/binary_tree_list/node.hpp
@@ -0,0 +1,66 @@
+
+#ifndef _BINARY_TREE_LIST_NODE
+
+  #define _BINARY_TREE_LIST_NODE
+
+  #include <stdint.h>
+
+
+  template <class class_type>
+  class BinaryTreeListNode {
+
+    private:
+
+      class_type value;
+      uint32_t index;
+
+      BinaryTreeListNode<class_type>* left;
+      BinaryTreeListNode<class_type>* right;
+
+
+    public:
+
+      BinaryTreeListNode() {
+        this->left = (BinaryTreeListNode<class_type>*)NULL;
+        this->right = (BinaryTreeListNode<class_type>*)NULL;
+
+        //this->value = NULL;
+      }
+
+      uint32_t getIndex() {
+        return this->index;
+      }
+
+      void setIndex(uint32_t idx) {
+        this->index = idx;
+      }
+
+      class_type getValue() {
+        return this->value;
+      }
+
+      void setValue(class_type value) {
+        this->value = value;
+      }
+
+      BinaryTreeListNode<class_type>* getLeft() {
+        return this->left;
+      }
+
+      void setLeft(BinaryTreeListNode<class_type>* node) {
+        this->left = node;
+      }
+
+      BinaryTreeListNode<class_type>* getRight() {
+        return this->right;
+      }
+
+      void setRight(BinaryTreeListNode<class_type>* node) {
+        this->right = node;
+      }
+
+  };
+
+#endif
+
+
