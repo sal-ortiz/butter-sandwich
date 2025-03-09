53f253a9ab3d0154ad6f90633ea597283906a80b
= <this.is.salvador.ortiz@gmail.com>
2025-03-09 17:03:12 -0500

    default to fixed tree list in list lib

--- lib/core/list.hpp
+++ lib/core/list.hpp
@@ -6,13 +6,14 @@
 
   #include "./list/linked_list.hpp"
   #include "./list/binary_tree_list.hpp"
-  //#include "./list/static_tree_list.hpp"
+  #include "./list/fixed_tree_list.hpp"
 
 
   template <class class_type>
   //class List: public LinkedList<class_type> {
-  //class List: public FixedBinaryTreeList<class_type> {
-  class List: public BinaryTreeList<class_type> {
+  //class List: public ArrayList<class_type> {
+  class List: public FixedTreeList<class_type> {
+  //class List: public BinaryTreeList<class_type> {
     // intentionally left blank.
   };
 
