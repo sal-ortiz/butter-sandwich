ea48d2c545f623125077a2e641a08e528fe42787
= <this.is.salvador.ortiz@gmail.com>
2025-03-21 18:41:02 -0500

    converted scale data lib into a struct

--- lib/runtime/data/scale.hpp
+++ lib/runtime/data/scale.hpp
@@ -3,26 +3,10 @@
 
   #define _SCALE_HPP
 
-
-  class Scale {
-
-    public:
-
-      float horz;
-      float vert;
-      float depth;
-
-      Scale(
-        float xScale=1.0,
-        float yScale=1.0,
-        float zScale=1.0
-      ) {
-
-        this->horz = xScale;
-        this->vert = yScale;
-        this->depth = zScale;
-      }
-
+  struct Scale {
+    float horz = 1.0;
+    float vert = 1.0;
+    float depth = 1.0;
   };
 
 #endif
