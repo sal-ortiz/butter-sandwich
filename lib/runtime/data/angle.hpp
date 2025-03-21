0fa905a93461e4335f3cb22424cddbecded47e81
= <this.is.salvador.ortiz@gmail.com>
2025-03-21 18:39:13 -0500

    converted angle data lib into a struct

--- lib/runtime/data/angle.hpp
+++ lib/runtime/data/angle.hpp
@@ -5,36 +5,12 @@
 
   #include "./position.hpp"
 
+  struct Angle {
+    Position center;
 
-  class Angle {
-
-    public:
-
-      float pitch;
-      float roll;
-      float yaw;
-
-      Position center;
-
-      Angle(
-        float pitch=0.0,
-        float roll=0.0,
-        float yaw=0.0,
-
-        float centerX=0.0,
-        float centerY=0.0,
-        float centerZ=0.0
-      ) {
-
-        this->pitch = pitch;
-        this->roll = roll;
-        this->yaw = yaw;
-
-        this->center.horz = centerX;
-        this->center.vert = centerY;
-        this->center.depth = centerZ;
-      }
-
+    float pitch = 0;
+    float roll = 0;
+    float yaw = 0;
   };
 
 #endif
