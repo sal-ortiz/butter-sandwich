89dcd25cfb6a2dd5f23bd30189d0b969bc2b0f5f
= <this.is.salvador.ortiz@gmail.com>
2025-03-21 18:40:10 -0500

    converted color data lib into a struct

--- lib/runtime/data/color.hpp
+++ lib/runtime/data/color.hpp
@@ -4,23 +4,11 @@
 
   #define _COLOR_HPP
 
-
-  class Color {
-
-    public:
-
-      float red;
-      float green;
-      float blue;
-      float alpha;
-
-      Color(float red=255, float green=255, float blue=255, float alpha=255) {
-        this->red = red;
-        this->green = green;
-        this->blue = blue;
-        this->alpha = alpha;
-      }
-
+  struct Color {
+    float red = 255;
+    float green = 255;
+    float blue = 255;
+    float alpha = 255;
   };
 
 #endif
