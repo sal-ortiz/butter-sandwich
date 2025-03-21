edf09fa754c60bfc10cb694c71cb43be22ae8a55
= <this.is.salvador.ortiz@gmail.com>
2025-03-21 18:42:14 -0500

    converted view data lib into a struct

--- lib/runtime/data/view.hpp
+++ lib/runtime/data/view.hpp
@@ -6,32 +6,9 @@
   #include "./position.hpp"
   #include "./size.hpp"
 
-
-  class View {
-
-    public:
-
-      Position position;
-      Size size;
-
-      View(
-        float xPos=0.0,
-        float yPos=0.0,
-        float zPos=0.0,
-        float width=0.0,
-        float height=0.0,
-        float depth=0.0
-      ) {
-
-        this->position.horz = xPos;
-        this->position.vert = yPos;
-        this->position.depth = zPos;
-
-        this->size.horz = width;
-        this->size.vert = height;
-        this->size.depth = depth;
-      }
-
+  struct View {
+    Position position;
+    Size size;
   };
 
 #endif
