c5685a53462919875aaf71e7529fef88a2917874
= <this.is.salvador.ortiz@gmail.com>
2025-03-21 18:41:49 -0500

    converted trajectory data lib into a struct

--- lib/runtime/data/trajectory.hpp
+++ lib/runtime/data/trajectory.hpp
@@ -8,40 +8,16 @@
   #include "./scale.hpp"
   #include "./color.hpp"
 
+  struct Trajectory {
+    Position position;
+    Angle angle;
+    Scale scale;
+    Color color;
 
-  class Trajectory {
-
-    public:
-
-      Position position;
-      Angle angle;
-      Scale scale;
-      Color color;
-
-      Position positionRate;
-      Angle angleRate;
-      Scale scaleRate;
-      Color colorRate;
-
-      Trajectory() {
-        this->positionRate.horz = 1.0;
-        this->positionRate.vert = 1.0;
-        this->positionRate.depth = 1.0;
-
-        this->angleRate.pitch = 1.0;
-        this->angleRate.roll = 1.0;
-        this->angleRate.yaw = 1.0;
-
-        this->scaleRate.horz = 1.0;
-        this->scaleRate.vert = 1.0;
-        this->scaleRate.depth = 1.0;
-
-        this->colorRate.red = 255.0;
-        this->colorRate.green = 255.0;
-        this->colorRate.blue = 255.0;
-        this->colorRate.alpha = 255.0;
-      }
-
+    Position positionRate;
+    Angle angleRate;
+    Scale scaleRate;
+    Color colorRate;
   };
 
 #endif
